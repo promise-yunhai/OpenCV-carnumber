64912de87a3014946a44deb34185cd749da561c4
promise <79844631+promise-yunhai@users.noreply.github.com>
2022-10-26 13:16:29 +0800

    Update img_math.py

--- img_math.py
+++ img_math.py
@@ -1,30 +1,30 @@
 import cv2
 import numpy as np
 
-Min_Area = 2000 # 车牌区域允许最大面积
+Min_Area = 2000 # Maximum area allowed for license plate area
 
 """
-    该文件包含读文件函数
-    取零值函数
-    矩阵校正函数
-    颜色判断函数
+    The file contains read file functions
+    Zero taking function
+    Matrix correction function
+    Color judgment function
 """
 
 def img_read(filename):
     '''
-        以uint8方式读取filename 
-        放入imdecode中，cv2.IMREAD_COLOR读取彩色照片
+        uint8 merhods read filename 
+        into imdecode，cv2.IMREAD_COLOR read color picture
     '''
 
-    #cv2.IMREAD_COLOR：读入一副彩色图像。图像的透明度会被忽略，这是默认参数
-    #cv2.IMREAD_GRAYSCALE：以灰度模式读入图像
-    #cv2.IMREAD_UNCHANGED：读入一幅图像，并且包括图像的 alpha 通道
+    #cv2.IMREAD_COLOR：Reads in a color image. The transparency of the image will be ignored, which is the default parameter
+    #cv2.IMREAD_GRAYSCALE：Read the image in grayscale mode.
+    #cv2.IMREAD_UNCHANGED：Read in an image and include the alpha channel of the image.
     return cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_COLOR)
     
 
 
 def find_waves(threshold, histogram):
-    up_point = -1  # 上升点
+    up_point = -1  # up plot
     is_peak = False
     if histogram[0] > threshold:
         up_point = 0
@@ -57,7 +57,7 @@ def accurate_place(card_img_hsv, limit1, limit2, color):
     yh = 0
     yl = row_num
     row_num_limit = 21
-    col_num_limit = col_num * 0.8 if color != "green" else col_num * 0.5  # 绿色有渐变
+    col_num_limit = col_num * 0.8 if color != "green" else col_num * 0.5  # green have gradually
     for i in range(row_num):
         count = 0
         for j in range(col_num):
@@ -88,25 +88,25 @@ def accurate_place(card_img_hsv, limit1, limit2, color):
 
 
 def img_findContours(img_contours):
-    # 查找轮廓
-    # cv2.findContours() 
-    # 有三个参数，第一个是输入图像，第二个是轮廓检索模式，第三个是轮廓近似方法。参数为二值图，即黑白的（不是灰度图）
-    # 返回值有三个，第一个是图像，第二个是轮廓，第三个是（轮廓的）层析结构。
-    # 轮廓（第二个返回值）是一个 Python列表，其中存储这图像中的所有轮廓。
-    # 每一个轮廓都是一个 Numpy 数组，包含对象边界点（x，y）的坐标。
+   # find outline
+# cv2.findContours()
+# There are three parameters, the first is the input image, the second is the contour retrieval mode, and the third is the contour approximation method. It is a binary graph of parameters, that is, black and white (not a gray graph).
+# There are three return values, the first is an image, the second is an outline, and the third is a (outline's) chromatographic structure.
+# Outline (the second return value) is a Python list, in which all outlines in this image are stored.
+# Each outline is a Numpy array containing the coordinates of the boundary points (x, y) of the object.
     contours, hierarchy = cv2.findContours(img_contours, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.RETR_TREE建立一个等级树结构的轮廓
-    #  cv2.CHAIN_APPROX_SIMPLE压缩水平方向，垂直方向，对角线方向的元素，
-    #  只保留该方向的终点坐标，例如一个矩形轮廓只需4个点来保存轮廓信息
+  # cv2.RETR_TREE Establish the outline of a hierarchical tree structure.
+# cv2. chain _ approximate _ simple      compresses elements in horizontal, vertical and diagonal directions,
+# Keep only the coordinates of the end point of this direction. For example, a rectangular outline only needs 4 points to save the outline information.
 
-    # cv2.contourArea计算该轮廓的面积
+# cv2.contourArea Calculate the area of the outline.
     contours = [cnt for cnt in contours if cv2.contourArea(cnt) > Min_Area]
     # print("findContours len = ", len(contours)) 
 
-    # 面积小的都筛选掉
+    # remove small area
     car_contours = []
     for cnt in contours:
-        ant = cv2.minAreaRect(cnt)# 得到最小外接矩形的（中心(x,y), (宽,高), 旋转角度）
+        ant = cv2.minAreaRect(cnt)# Get the (center (x,y), (width, height), rotation angle) of the minimum circumscribed rectangle.
         width, height = ant[1]
         if width < height:
             width, height = height, width
@@ -114,25 +114,24 @@ def img_findContours(img_contours):
 
         if 2 < ration < 5.5:
             car_contours.append(ant)
-            # box = cv2.boxPoints(ant) # 获得要绘制这个矩形的 4 个角点
-
+            # box = cv2.boxPoints(ant) # Get the four corners of the rectangle to draw.
     return car_contours
 
 
 def img_Transform(car_contours, oldimg, pic_width, pic_hight):
     """
-    进行矩形矫正
+   Rectangular correction
     """
     car_imgs = []
-    for car_rect in car_contours: #（中心(x,y), (宽,高), 旋转角度）
+    for car_rect in car_contours: #(center (x,y), (width, height), rotation angle) 
         if -1 < car_rect[2] < 1: 
             angle = 1
-            # 对于角度为-1 1之间时，默认为1
+            # For angles between-1 and 1, the default is1
         else:
             angle = car_rect[2]
         car_rect = (car_rect[0], (car_rect[1][0] + 5, car_rect[1][1] + 5), angle)
 
-        box = cv2.boxPoints(car_rect) # 获得要绘制这个矩形的 4 个角点
+        box = cv2.boxPoints(car_rect) # Get the four plots of the rectangle to draw.
 
         heigth_point = right_point = [0, 0]
         left_point = low_point = [pic_width, pic_hight]
@@ -147,11 +146,11 @@ def img_Transform(car_contours, oldimg, pic_width, pic_hight):
             if right_point[0] < point[0]:
                 right_point = point
 
-        if left_point[1] <= right_point[1]:  # 正角度
+        if left_point[1] <= right_point[1]:  # +angle
             new_right_point = [right_point[0], heigth_point[1]]
-            pts2 = np.float32([left_point, heigth_point, new_right_point])  # 字符只是高度需要改变
+            pts2 = np.float32([left_point, heigth_point, new_right_point])  # Only the character height needs to be changed.
             pts1 = np.float32([left_point, heigth_point, right_point])
-            #  仿射变换
+           
             M = cv2.getAffineTransform(pts1, pts2)
             dst = cv2.warpAffine(oldimg, M, (pic_width, pic_hight))
 
@@ -162,9 +161,9 @@ def img_Transform(car_contours, oldimg, pic_width, pic_hight):
             car_img = dst[int(left_point[1]):int(heigth_point[1]), int(left_point[0]):int(new_right_point[0])]
             car_imgs.append(car_img)
 
-        elif left_point[1] > right_point[1]:  # 负角度
+        elif left_point[1] > right_point[1]:  # -angle
             new_left_point = [left_point[0], heigth_point[1]]
-            pts2 = np.float32([new_left_point, heigth_point, right_point])  # 字符只是高度需要改变
+            pts2 = np.float32([new_left_point, heigth_point, right_point])  #Only the character height needs to be changed.
             pts1 = np.float32([left_point, heigth_point, right_point])
             M = cv2.getAffineTransform(pts1, pts2)
             dst = cv2.warpAffine(oldimg, M, (pic_width, pic_hight))
@@ -178,7 +177,7 @@ def img_Transform(car_contours, oldimg, pic_width, pic_hight):
 
 def img_color(card_imgs):
     """
-    颜色判断函数
+    Color judgment function
     """
     colors = []
     for card_index, card_img in enumerate(card_imgs):
@@ -187,7 +186,7 @@ def img_color(card_imgs):
         try:
             card_img_hsv = cv2.cvtColor(card_img, cv2.COLOR_BGR2HSV)
         except:
-            print("矫正矩形出错, 转换失败")# 可能原因:上面矫正矩形出错
+            print("矫正矩形出错, 转换失败")# Possible cause: Error in correcting rectangle above.
         
         if card_img_hsv is None:
             continue
@@ -216,7 +215,7 @@ def img_color(card_imgs):
         if yello * 2 >= card_img_count:
             color = "yellow"
             limit1 = 11
-            limit2 = 34  # 有的图片有色偏偏绿
+            limit2 = 34  # Some pictures are colored but green.
         elif green * 2 >= card_img_count:
             color = "green"
             limit1 = 35
@@ -224,7 +223,7 @@ def img_color(card_imgs):
         elif blue * 2 >= card_img_count:
             color = "blue"
             limit1 = 100
-            limit2 = 124  # 有的图片有色偏偏紫
+            limit2 = 124  # Some pictures are colored but purple.
         elif black + white >= card_img_count * 0.7:
             color = "bw"
         colors.append(color)
@@ -276,7 +275,7 @@ def img_color(card_imgs):
 
 def seperate_card(img, waves):
     """
-    分离车牌字符
+    Separating license plate characters
     """
     h , w = img.shape
     part_cards = []
@@ -286,7 +285,7 @@ def seperate_card(img, waves):
         part_cards.append(img[:, wave[0]:wave[1]])
         chrpic = img[0:h,wave[0]:wave[1]]
         
-        #保存分离后的车牌图片
+        #Saved license plate picture
         cv2.imwrite('tmp/chechar{}.jpg'.format(i),chrpic)
     
 
